54da622bdd112a55752220a74d58aa7f4b3601c3
Julio Souza <dev.js.programmer@gmail.com>
2023-05-24 11:50:56 -0400

    Adicionado as classes de AgendamentoController

--- controller/AgendamentoController.java
+++ controller/AgendamentoController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import java.time.LocalDateTime;
+
 public class AgendamentoController {
+
+    public Funcionario buscarFuncionario(int matricula) {
+        
+    }
+
+    public boolean consultaDisponibilidadeHorario(int nroMatricula) {
+        return true;
+    }
+
+    public boolean buscarChamado(int codChamado) {
+        return true;
+    }
+
+    public void registrarAgendamento(Chamado chamado, LocalDateTime horario, Funcionario funcionario, String nomeRes, String telefoneRes, ) {
+
+    }
 }
