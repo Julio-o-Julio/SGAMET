62d600bc6a573dbcf3fecf8c031315b2c333082a
edum3lo <eduardomelonunes2001@gmail.com>
2023-05-25 17:24:15 -0300

    att model

--- view/actions/AgendamentoViewCRUDactions.java
+++ view/actions/AgendamentoViewCRUDactions.java
@@ -2,8 +2,15 @@ package view.actions;
 
 
 import javax.swing.*;
+
+import controller.AgendamentoController;
+import model.Chamado;
+import model.Funcionario;
+import view.validation.HorarioValidador;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
 
 public class AgendamentoViewCRUDactions implements ActionListener {
     JTextField nomeField;
@@ -39,7 +46,26 @@ public class AgendamentoViewCRUDactions implements ActionListener {
         System.out.println( e);
 
         if(e.getSource().equals(btnAgendar)){
-            System.out.println("AGENDADO");
+            int codChamado = Integer.parseInt( codChamadoField.getText() );
+            if(!AgendamentoController.existeChamado(codChamado)){
+                System.out.println("Chamado não existe!");
+                return;
+            }
+            Chamado chamado = AgendamentoController.buscarChamado(codChamado);
+
+            int matriculaFuncionario = Integer.parseInt(matriculaFuncionarioField.getText());
+            if(!AgendamentoController.existeFuncionario(matriculaFuncionario)){
+                System.out.println("Nenhum funcionário pôde ser encontrado a partir da matrícula informada");
+                return;
+            }
+            Funcionario funcionario = AgendamentoController.buscarFuncionario(matriculaFuncionario);
+            String nomeRes = nomeField.getText();
+            String telefoneRes = telefoneField.getText();
+            if(!HorarioValidador.ehValido(horarioField)){
+                return;
+            }
+            LocalDateTime horario = HorarioValidador.getValor(horarioField);
+            AgendamentoController.registrarAgendamento(chamado, horario, funcionario, nomeRes, telefoneRes); 
         }
         else if(e.getSource().equals(btnAtualizar)){
             System.out.println("Atualizado!!");
