74cb68099226456e0d425ca807cfc331650bcfe3
Julio Souza <dev.js.programmer@gmail.com>
2023-05-24 20:23:06 -0400

    Atualizando o metodo consultaDisponibilidadeHorarios

--- controller/AgendamentoController.java
+++ controller/AgendamentoController.java
@@ -2,21 +2,42 @@ package controller;
 
 import java.time.LocalDateTime;
 
+import dao.FuncionarioDAO;
+import dao.ChamadoDAO;
 import model.Funcionario;
 import model.Chamado;
 
 public class AgendamentoController {
 
+    Funcionario funcionario;
+
     public Funcionario buscarFuncionario(int matricula) {
-        return null;
+        return pesquisarFuncionario(matricula);
     }
 
-    public boolean consultaDisponibilidadeHorario(int nroMatricula) {
+    public boolean consultaDisponibilidadeHorarios(int nroMatricula, LocalDateTime horarioDesejado) {
+
+        funcionario = this.buscarFuncionario(nroMatricula);
+
+        for (LocalDateTime horario : funcionario.getHorAgendados()) {
+            LocalDateTime inicioIntervalo = horarioDesejado.minusHours(1);
+            LocalDateTime fimIntervalo = horarioDesejado.plusHours(1);
+
+            if (horario.isAfter(inicioIntervalo) && horario.isBefore(fimIntervalo)) {
+                return false;
+            }
+        }
+
         return true;
     }
 
     public boolean buscarChamado(int codChamado) {
-        return true;
+
+        for (Chamado chamado : chamados) {
+            return true;
+        }
+
+        return false;
     }
 
     public void registrarAgendamento(Chamado chamado, LocalDateTime horario, Funcionario funcionario, String nomeRes, String telefoneRes) {
